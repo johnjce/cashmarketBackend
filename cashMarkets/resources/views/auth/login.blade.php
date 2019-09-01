15c465232c28c69dfad2a3ebce5a20604005b138
jhonts <jjcinformatik@gmail.com>
2019-09-01 18:31:03 +0100

    changed!! login page, a most beautiful view

--- cashMarkets/resources/views/auth/login.blade.php
+++ cashMarkets/resources/views/auth/login.blade.php
@@ -1,85 +1,91 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <title>CashMarketsV2.0-Login</title>
-    <meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no">
-    <link rel="shortcut icon" href="favicon_16.ico" />
-    <link rel="bookmark" href="favicon_16.ico" />
-    <!-- site css -->
-    <link rel="stylesheet" href="dist/css/site.min.css">
-    <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,800,700,400italic,600italic,700italic,800italic,300italic" rel="stylesheet" type="text/css">
-    <!-- <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'> -->
-    <!-- HTML5 shim, for IE6-8 support of HTML5 elements. All other JS at the end of file. -->
-    <!--[if lt IE 9]>
-      <script src="js/html5shiv.js"></script>
-      <script src="js/respond.min.js"></script>
-    <![endif]-->
-    <script type="text/javascript" src="dist/js/site.min.js"></script>
-    <style>
-        body {
-            padding-top: 40px;
-            padding-bottom: 40px;
-            background-color: #303641;
-            color: #C1C3C6
-        }
-    </style>
+    <title>Login V12</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--===============================================================================================-->
+    <link rel="icon" type="image/png" href="images/icons/favicon.ico" />
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="css/util.css">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <!--===============================================================================================-->
 </head>
 
 <body>
-    <div class="container">
-        <div class="row">
-            <div class="Absolute-Center is-Responsive">
-                <div id="logo-container"></div>
-                <div class="col-sm-12 col-md-10 col-md-offset-1">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-                        <div class="form-group input-group">
-                            <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                            <input id="user" type="text" class="form-control @error('user') is-invalid @enderror" name="user" value="{{ old('user') }}" required autocomplete="user" autofocus>
 
-                            @error('user')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                            @enderror
-                        </div>
-                        <div class="form-group input-group">
-                            <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+    <div class="limiter">
+        <div class="container-login100" style="background-image: url('images/img-01.jpg');">
+            <div class="wrap-login100 p-t-190 p-b-30">
+                <form method="POST" action="{{ route('login') }}" class="login100-form validate-form">
+                    @csrf
+                    <div class="login100-form-avatar">
+                        <img src="https://picsum.photos/200" alt="AVATAR">
+                    </div>
 
-                            @error('password')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                            @enderror
-                        </div>
-                        <div class="form-group">
-                            <button class="btn btn-lg btn-primary btn-block" type="submit">Entrar</button>
-                        </div>
-                    </form>
-                </div>
+                    <span class="login100-form-title p-t-20 p-b-45">
+                        CashMarkets V2.0
+                    </span>
+
+                    <div class="wrap-input100 validate-input m-b-10" data-validate="Username is required">
+                        <input id="user" type="text" class="input100 form-control @error('user') is-invalid @enderror" name="user" value="{{ old('user') }}" required autocomplete="user" autofocus>
+                        <span class="focus-input100"></span>
+                        <span class="symbol-input100">
+                            <i class="fa fa-user"></i>
+                        </span>
+                        @error('user')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </div>
+
+                    <div class="wrap-input100 validate-input m-b-10" data-validate="Password is required">
+                        <input id="password" type="password" class="input100 form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                        <span class="focus-input100"></span>
+                        <span class="symbol-input100">
+                            <i class="fa fa-lock"></i>
+                        </span>
+                        @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </div>
+
+                    <div class="container-login100-form-btn p-t-10">
+                        <button class="login100-form-btn btn btn-lg btn-primary btn-block" type="submit">Entrar</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
-    <div class="clearfix"></div>
-    <br><br>
-    <!--footer-->
-    <footer class="page-footer font-small blue fixed-bottom">
 
-        <!-- Copyright -->
-        <div class="footer-copyright text-center py-3">
 
-            Copyright &copy;
-            <script>
-                document.write(new Date().getFullYear());
-            </script> Todos los derechos reservados Cashmartkets | </i> by <a href="https://johnjce.github.io/inicio/" target="_blank" class="text-primary">..::jhonts::..</a>
 
-        </div>
-        <!-- Copyright -->
 
-    </footer>
+    <!--===============================================================================================-->
+    <script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="vendor/bootstrap/js/popper.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="vendor/select2/select2.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="js/main2.js"></script>
 
 </body>
 
