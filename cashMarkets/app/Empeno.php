3e8f8ccbbda3ef65e11b8a417b08535eaf8c1871
jhonts <jjcinformatik@gmail.com>
2019-09-02 22:14:10 +0100

    changed models for corrects

--- cashMarkets/app/Empeno.php
+++ cashMarkets/app/Empeno.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Empenos extends Model
+class Empeno extends Model
 {
     //
 }
--- cashMarkets/app/Producttype.php
+++ cashMarkets/app/Producttype.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Producttypes extends Model
+class Producttype extends Model
 {
     //
 }
